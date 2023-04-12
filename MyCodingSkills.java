fc1098756b7ba7d1859a45daf7c118d547a20095
aGonzalezAppDev <agonzalez@appdev.yearup.org>
2023-04-12 10:10:12 -0400

    Added a new Java class

---
+++ MyCodingSkills.java
@@ -0,0 +1,12 @@
+public class MyCodingSkills {
+    public static void main(String[] args) {
+        System.out.println("Java");
+        System.out.println("SQL");
+        System.out.println("Python");
+        System.out.println("C++");
+        System.out.println("C");
+        System.out.println("CSS");
+        System.out.println("HTML");
+        System.out.println("Javascript");
+    }
+}
